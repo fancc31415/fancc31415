a349de5ee1575900a0994137f136aff9127ac4b1
137******43 <329139437@qq.com>
2025-05-15 13:40:22 +0800

    5

--- music.js
+++ music.js
@@ -6,7 +6,6 @@ const playlistId = '6kweM7ZEesnIFkXnNd7oaM';
   width="100%"
   height="100%"
   style={{ minHeight: '360px' }}
-  frameBorder="0"
   allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
   loading="lazy"
 />
