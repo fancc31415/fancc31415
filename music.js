2bf76b5557900a2e976b1a3137d0c0e4935cab25
137******43 <329139437@qq.com>
2025-05-15 13:17:35 +0800

    4

---
+++ music.js
@@ -0,0 +1,12 @@
+const playlistId = '6kweM7ZEesnIFkXnNd7oaM';
+
+<iframe
+  title="Spotify Embed: Recommendation Playlist "
+  src={`https://open.spotify.com/embed/playlist/6kweM7ZEesnIFkXnNd7oaM?utm_source=generator&theme=0`}
+  width="100%"
+  height="100%"
+  style={{ minHeight: '360px' }}
+  frameBorder="0"
+  allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+  loading="lazy"
+/>
